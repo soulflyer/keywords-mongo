4234dac09164773362fc63862f9aa76a50df8af4
Iain Wood <iain@soulflyer.co.uk>
2015-11-12 23:12:51 +0700

    Added Tuan's make-list function

--- src/keywords_mongo/core.clj
+++ src/keywords_mongo/core.clj
@@ -7,10 +7,35 @@
   (:import [com.mongodb MongoOptions ServerAddress])
   (:gen-class))
 
+(defn lines [filename]
+  (with-open [rdr (io/reader filename)]
+    (doall (line-seq rdr))))
+
+(defn count-tabs [s]
+  (count (take-while #(= \tab %) s)))
+
+(defn make-list [filename]
+  "Creates a vector of entries consisting of a keyword and an array of sub-keywords"
+  (loop [lines (lines filename) res []]
+    (if (nil? lines)
+      res
+      (let [[cur & more] lines
+            cur-level    (count-tabs cur)]
+        (recur (next lines)
+               (conj res
+                     (->> more
+                          (take-while #(> (count-tabs %) cur-level))
+                          (filter #(= (count-tabs %) (inc cur-level)))
+                          (map str/trim)
+                          vec
+                          list
+                          (cons (str/trim cur)))))))))
+
+
 (defn save-keywords
   "saves all the keywords in file to db"
   [dbase collection file]
-  (println file))
+  (make-list file))
 
 (defn -main [& args]
   (save-keywords "monger-test" "keywords" (first args)))
